4420b010510ec01d67d63c43133ca76c77dbc264
Nyantai12 <112607105+Nyantai12@users.noreply.github.com>
2025-09-09 17:44:12 +0800

    ecommerce template done
    .

---
+++ ecommerce/shop_app/views.py
@@ -0,0 +1,29 @@
+from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404, render
+from .models import Product  # <- Үүнийг заавал нэмнэ
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+def cart_view(request):
+    return render(request, 'cart.html')
+
+def store(request):
+    return render(request, 'store.html', )
+def signin(request):
+    return render(request, 'signin.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def product_detail(request, ):
+    return render(request, 'product-detail.html',)
+
+def place_order(request):
+    return render(request,'place.html') 
+
+def add_to_cart(request, ):
+    return render(request, 'cart.html')
+def password_reset_view(request):
+    return render(request, 'password_reset.html')
